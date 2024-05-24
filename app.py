e0216fcc3a3fa5d885dca9ed0b521de2c302f1e0
Pratham <prathamnjoshi@gmail.com>
2024-05-24 12:59:18 +0530

    khup kahi changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import requests
+import os
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -36,24 +36,12 @@ X = data[features]
 y = data["Manufacturer"]
 
 # Split data into train and test sets
-# Adjust test_size and random_state as needed
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize and train the model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-# Function to get image URL from Unsplash
-def get_image_url_from_unsplash(query):
-    access_key = 'VY2QYbIG-3b2lEhgWBod9dpOqFwtohLa1pIi3KEOYMA'
-    url = f'https://api.unsplash.com/photos/random?query={query}&client_id={access_key}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()['urls']['regular']
-    else:
-        return None
-
-# Define routes
 @app.route('/')
 def index():
     return render_template('car.html')
@@ -71,16 +59,23 @@ def get_recommendations():
         # Ensure that there are no missing values in the input
         if user_input.isnull().values.any():
             return "Please fill all fields."
-        
+
         predicted_make = model.predict(user_input)[0]
         recommended_cars = data[data['Manufacturer'] == predicted_make].sort_values(by=features, ascending=False).head(5)
 
+        # Save user input to an Excel file
+        if os.path.exists('user_preferences.xlsx'):
+            existing_data = pd.read_excel('user_preferences.xlsx')
+            updated_data = pd.concat([existing_data, user_input], ignore_index=True)
+        else:
+            updated_data = user_input
+
+        updated_data.to_excel('user_preferences.xlsx', index=False)
+
         # Add image URLs to the DataFrame based on Unsplash
-        recommended_cars['ImageURL'] = recommended_cars.apply(lambda row: get_image_url_from_unsplash(f"{row['Name']} {row['Manufacturer']} car"), axis=1)
+        # recommended_cars['ImageURL'] = recommended_cars.apply(lambda row: get_image_url_from_unsplash(f"{row['Name']} {row['Manufacturer']} car"), axis=1)
 
         return render_template('recommendations.html', recommendations=recommended_cars)
-    
-    print(data.isnull().sum())
 
 @app.route('/home')
 def home():
